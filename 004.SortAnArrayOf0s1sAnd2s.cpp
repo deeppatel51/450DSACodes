03b55757ce26e281cff366280dad7e34f25a86bb
deeppatel51 <88723958+deeppatel51@users.noreply.github.com>
2022-04-10 16:41:48 +0530

    Create 004.SortAnArrayOf0s1sAnd2s.cpp

---
+++ 004.SortAnArrayOf0s1sAnd2s.cpp
@@ -0,0 +1,68 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        // code here
+        int z=0,o=0,t=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==0)
+            z++;
+            if(a[i]==1)
+            o++;
+            if(a[i]==2)
+            t++;
+        }
+   
+        for(int i=0;i<z;i++)
+        {
+            a[i]=0;
+        }
+        for(int i=z;i<z+o;i++)
+        {
+            a[i]=1;
+        }
+        for(int i=z+o;i<z+o+t;i++)
+        {
+            a[i]=2;
+        }
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
